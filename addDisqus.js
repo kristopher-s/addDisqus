2eb3f1c72a28598abcbca811e00acf8559836145
Kris <34043455+kristopher-s@users.noreply.github.com>
2021-10-18 11:35:39 -0400

    Update addDisqus.js

--- addDisqus.js
+++ addDisqus.js
@@ -1,4 +1,4 @@
-//START: Disqus integration
+//START: Disqus
 Answers.onPageLoad(function (event, data) {
     if (data.pageType === 20) {
   
@@ -10,10 +10,11 @@ Answers.onPageLoad(function (event, data) {
   
   (function() { // DON'T EDIT BELOW THIS LINE
     var d = document, s = d.createElement('script');
+    // REPLACE s.src BELOW with your Disqus embed URL
     s.src = 'https://platypusplayground.disqus.com/embed.js';
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
     })();
   }
 });
-//END: Disqus integration
+//END: Disqus
