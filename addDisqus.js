c2fe8b372aa6b306a85fece43bf25065f12bc5be
Kris <34043455+kristopher-s@users.noreply.github.com>
2021-10-18 11:33:47 -0400

    Update addDisqus.js

--- addDisqus.js
+++ addDisqus.js
@@ -1,32 +1,19 @@
 //START: Disqus integration
-
 Answers.onPageLoad(function (event, data) {
-  // Page loaded! All dom elements are visible now.
     if (data.pageType === 20) {
-      console.log(event);
   
-  //Disqus config vars for article-specific comments.
-  //const disqus_config = function () { 
-  //this.page.url = answersSdk.getVisiblePageData().article.url;
-  //this.page.identifier = answersSdk.getVisiblePageData().article.id;
+  //Disqus config vars
+  const disqus_config = function () { 
+  this.page.url = event.article.url;
+  this.page.identifier = event.article.id;
   };
   
-  //Targets DOM node after where we will insert Disqus container.
-  const targetNode = document.querySelector(".feedback-section-container");
-    
-  //Creates DIV where Disqus container will reside.
-  const createDisqusContainer = document.createElement('div')
-  createDisqusContainer.setAttribute("id", "disqus_thread")
-  targetNode.appendChild(createDisqusContainer)
-
-  //Loads Disqus script.
-  const selectDisqusContainer = document.querySelector("#disqus_thread")
-  const insertDisqus = document.createElement('script')
-  insertDisqus.src = ''; //Add account specific Disqus script URL e.g. https://<YOUR_DOMAIN>.disqus.com/embed.js
-  insertDisqus.setAttribute('data-timestamp', +new Date());
-  targetNode.appendChild(insertDisqus)
-
+  (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://platypusplayground.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
   }
 });
-
 //END: Disqus integration
